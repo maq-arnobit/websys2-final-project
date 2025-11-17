83224f8fce84717a637759ead1638e05ede4f913
Val Kenneth Ibrao <ibrao_v@f213.ub.lan>
2025-11-17 17:27:30 +0800

    Project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 4200;
+const usersRoutes = require('./src/routes/users_routes');
+
+
+app.use(express.json);
+
+app.use('/users', usersRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
