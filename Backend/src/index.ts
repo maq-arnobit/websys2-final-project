4064c54898ee132cdac148a9427cbfa0e83c14b8
1Rai1 <soronioclayton42@gmail.com>
2025-11-24 17:13:44 +0800

    DATABASE

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 4200;
-const usersRoutes = require('./src/routes/users_routes');
+const PORT = process.env.PORT || 3000;
+//const usersRoutes = require('./src/routes/users_routes');
 
 
 app.use(express.json);
 
-app.use('/users', usersRoutes)
+//app.use('/users', usersRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
