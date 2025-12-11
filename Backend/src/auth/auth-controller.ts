079c8b84d50b7b01b52cfc7ba1136c610aef7548
maq-arnobit <20237713@s.ubaguio.edu>
2025-12-12 01:42:37 +0800

    h

--- Backend/src/auth/auth-controller.ts
+++ Backend/src/auth/auth-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import passport from '../passport/passport';
+import passport from '../index';
 import { hashPassword } from '../utils/utils';
 import { AuthRequest } from './auth-middleware';
 
