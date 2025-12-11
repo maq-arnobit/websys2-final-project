51f0087201c49d81d93708b9ff2337bbf4d4b7b0
maq-arnobit <20237713@s.ubaguio.edu>
2025-12-12 01:41:05 +0800

    g

--- Backend/src/passport/passport.ts
+++ Backend/src/passport/passport.ts
@@ -1,7 +1,7 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import { comparePassword } from '../utils/utils';
-import db from '../../../models'; 
+import db from '../../models'; 
 
 interface ModelConfig {
   model: any;
